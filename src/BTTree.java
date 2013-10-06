ab870249f68983e2b3347b50bbc7e0862842cb36
Mustafa Mukadam <mhmukadam@yahoo.com>
2013-10-06 14:03:22 -0500

    Basic data structure Tree and TreeNodes

---
+++ src/BTTree.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class BTTree {
+	public BTTreeNode root;
+	public BTTreeNode solution;
+	
+	public BTTree(List<Integer> m) {
+		this.root.timeSlots = new ArrayList<Integer[]>();
+		this.root.meetings = new ArrayList<Integer>(m);
+	}
+	
+	public boolean backTracking() {
+		return false;
+	}
+}
--- src/BTTreeNode.java
+++ src/BTTreeNode.java
@@ -1,4 +1,13 @@
+import java.util.*; 
 
 public class BTTreeNode {
-
+	public List<Integer[]> timeSlots;
+	public List<Integer> meetings;
+	public BTTreeNode parent;
+	
+	public BTTreeNode(List<Integer[]> ts, List<Integer> m, BTTreeNode p) {
+		this.timeSlots = new ArrayList<Integer[]>(ts);
+		this.meetings = new ArrayList<Integer>(m);
+		this.parent = p;
+	}
 }
