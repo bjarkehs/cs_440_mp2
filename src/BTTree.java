e97badf4586d82fb4f7886b78c4395b43b11556a
Mustafa Mukadam <mhmukadam@yahoo.com>
2013-10-10 11:01:41 -0500

    List of list for timeSlots now

--- src/BTTree.java
+++ src/BTTree.java
@@ -6,11 +6,15 @@ public class BTTree {
 	
 	public BTTree(int t, PriorityQueue<Integer> m) {
 		//System.out.println("t = "+t+" , m = "+m);
-		this.root.timeSlots = new ArrayList<Integer[]>(t);
+		this.root.timeSlots = new ArrayList<List<Integer>>();
+		//create timeSlots of a specific size
+		for (int i = 0; i < t; i++) {
+			this.root.timeSlots.add(0);
+		}
 		this.root.meetings = m;
 	}
 	
-	public boolean checkAssignment(int m, int loc, Constraints c) {
+	public boolean checkAssignment(int m, int loc, List<List<Integer>> ts, Constraints c) {
 		return false;
 	}
 
@@ -22,8 +26,9 @@ public class BTTree {
 		int m = n.meetings.poll();
 		//System.out.println(m);
 		for (int i = 1; i<=n.timeSlots.size(); i++) {
-			if (checkAssignment(m,i,c)) {
-				
+			if (checkAssignment(m,i,n.timeSlots,c)) {
+				Integer[] temp = n.timeSlots.get()
+				n.timeSlots.add
 			}
 		}
 		
--- src/BTTreeNode.java
+++ src/BTTreeNode.java
@@ -1,17 +1,17 @@
 import java.util.*; 
 
 public class BTTreeNode {
-	public List<Integer[]> timeSlots;
+	public List<List<Integer>> timeSlots;
 	public PriorityQueue<Integer> meetings;
 	public BTTreeNode parent;
 	
 	public BTTreeNode() {
-		this.timeSlots = new ArrayList<Integer[]>();
+		this.timeSlots = new ArrayList<List<Integer>>();
 		this.meetings = new PriorityQueue<Integer>();
 	}
 	
-	public BTTreeNode(List<Integer[]> ts, PriorityQueue<Integer> m, BTTreeNode p) {
-		this.timeSlots = new ArrayList<Integer[]>(ts);
+	public BTTreeNode(List<List<Integer>> ts, PriorityQueue<Integer> m, BTTreeNode p) {
+		this.timeSlots = new ArrayList<List<Integer>>(ts);
 		this.meetings = new PriorityQueue<Integer>(m);
 		this.parent = p;
 	}
